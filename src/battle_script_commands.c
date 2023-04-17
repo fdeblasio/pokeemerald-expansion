c3659d7a005d5395b6a427e8f7fb7e6bbc48dbcb
Frank <fdeblasio1004@gmail.com>
2023-04-17 13:32:39 -0400

    Removed TMs from Pickup

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -1279,11 +1279,11 @@ static const u16 sRarePickupItems[] =
     ITEM_FULL_RESTORE,
     ITEM_ETHER,
     ITEM_WHITE_HERB,
-    ITEM_TM44_REST,
     ITEM_ELIXIR,
-    ITEM_TM01_FOCUS_PUNCH,
+    ITEM_MAX_ELIXIR,
+    ITEM_DESTINY_KNOT,
     ITEM_LEFTOVERS,
-    ITEM_TM26_EARTHQUAKE,
+    ITEM_BIG_NUGGET,
 };
 
 static const u8 sPickupProbabilities[] =

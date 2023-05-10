7cde5ee9637464cf8e06e41ca456e5c36f399cd2
Frank <fdeblasio1004@gmail.com>
2023-05-10 14:41:58 -0400

    Made outbreaks even more likely to occur

--- src/tv.c
+++ src/tv.c
@@ -1637,7 +1637,7 @@ static void TryStartRandomMassOutbreak(void)
 
     if (FlagGet(FLAG_SYS_GAME_CLEAR))
     {
-        if (!rbernoulli(1, 100))
+        if (!rbernoulli(1, 50))
         {
             sCurTVShowSlot = FindFirstEmptyNormalTVShowSlot(gSaveBlock1Ptr->tvShows);
             if (sCurTVShowSlot != -1)

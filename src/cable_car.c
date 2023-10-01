548017913603ee93ab198e141ac3788f7f0ad10e
Frank <fdeblasio1004@gmail.com>
2023-10-01 17:53:01 -0400

    Updated cable car

--- src/cable_car.c
+++ src/cable_car.c
@@ -795,11 +795,14 @@ static void CreateCableCarSprites(void)
         [FEMALE] = OBJ_EVENT_GFX_RIVAL_MAY_NORMAL
     };
     u16 rval = Random();
-    u16 hikerGraphicsIds[4] = {
+    u16 hikerGraphicsIds[7] = {
         OBJ_EVENT_GFX_HIKER,
         OBJ_EVENT_GFX_CAMPER,
         OBJ_EVENT_GFX_PICNICKER,
-        OBJ_EVENT_GFX_ZIGZAGOON_1
+        OBJ_EVENT_GFX_ZIGZAGOON_1,
+        OBJ_EVENT_GFX_POOCHYENA,
+        OBJ_EVENT_GFX_BLACK_BELT,
+        OBJ_EVENT_GFX_GIRL_3
     };
     s16 hikerCoords[2][2] = {
         {   0,  80 }, // Going up
@@ -877,8 +880,8 @@ static void CreateCableCarSprites(void)
         gSprites[spriteId].y2 = 8;
     }
 
-    // 1/64 chance for an NPC to appear hiking on the ground below the Cable Car
-    if ((rval % 64) == 0)
+    // 1/8 chance for an NPC to appear hiking on the ground below the Cable Car
+    if ((rval % 8) == 0)
     {
         // BUGFIX: The - 1 in the below ARRAY_COUNT means the Zigzagoon is never used
 #ifdef BUGFIX

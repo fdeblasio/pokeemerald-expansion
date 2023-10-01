0d3a64818e00785c5cb893c385ba705eec5048d9
Frank <fdeblasio1004@gmail.com>
2023-10-01 19:21:25 -0400

    Updated Steven's partner team

--- src/data/partner_parties.h
+++ src/data/partner_parties.h
@@ -1,26 +1,26 @@
 static const struct TrainerMon sParty_StevenPartner[] = {
     {
-        .species = SPECIES_METANG,
+        .species = SPECIES_MAWILE,
         .lvl = 42,
-        .nature = TRAINER_PARTY_NATURE(NATURE_BRAVE),
+        .nature = TRAINER_PARTY_NATURE(NATURE_ADAMANT),
         .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
-        .ev = TRAINER_PARTY_EVS(0, 252, 252, 0, 6, 0),
-        .moves = {MOVE_LIGHT_SCREEN, MOVE_PSYCHIC, MOVE_REFLECT, MOVE_METAL_CLAW},
-    },
-    {
-        .species = SPECIES_SKARMORY,
-        .lvl = 43,
-        .nature = TRAINER_PARTY_NATURE(NATURE_IMPISH),
-        .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
-        .ev = TRAINER_PARTY_EVS(252, 0, 0, 0, 6, 252),
-        .moves = {MOVE_TOXIC, MOVE_AERIAL_ACE, MOVE_PROTECT, MOVE_STEEL_WING},
+        .ev = TRAINER_PARTY_EVS(0, 252, 6, 252, 0, 0),
+        .moves = {MOVE_IRON_HEAD, MOVE_CRUNCH, MOVE_SWORDS_DANCE, MOVE_IRON_DEFENSE},
     },
     {
         .species = SPECIES_AGGRON,
+        .lvl = 43,
+        .nature = TRAINER_PARTY_NATURE(NATURE_IMPISH),
+        .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
+        .ev = TRAINER_PARTY_EVS(6, 252, 252, 0, 0, 0),
+        .moves = {MOVE_IRON_HEAD, MOVE_ROCK_SLIDE, MOVE_BODY_PRESS, MOVE_HARDEN},
+    },
+    {
+        .species = SPECIES_METANG,
         .lvl = 44,
         .nature = TRAINER_PARTY_NATURE(NATURE_ADAMANT),
         .iv = TRAINER_PARTY_IVS(31, 31, 31, 31, 31, 31),
-        .ev = TRAINER_PARTY_EVS(0, 252, 0, 0, 252, 6),
-        .moves = {MOVE_THUNDER, MOVE_PROTECT, MOVE_SOLAR_BEAM, MOVE_DRAGON_CLAW},
+        .ev = TRAINER_PARTY_EVS(6, 252, 252, 0, 0, 0),
+        .moves = {MOVE_BULLET_PUNCH, MOVE_ZEN_HEADBUTT, MOVE_PURSUIT, MOVE_MAGNET_RISE},
     }
 };

b1f5ddbcfa7b5e11e4ff2ae9f12fa0f490cc7529
Frank <fdeblasio1004@gmail.com>
2023-10-19 17:34:05 -0400

    More buffs

--- src/battle_util.c
+++ src/battle_util.c
@@ -9168,7 +9168,7 @@ static inline u32 CalcMoveBasePowerAfterModifiers(u32 move, u32 battlerAtk, u32 
         break;
     case ABILITY_IRON_FIST:
         if (gMovesInfo[move].punchingMove)
-           modifier = uq4_12_multiply(modifier, UQ_4_12(1.2));
+           modifier = uq4_12_multiply(modifier, UQ_4_12(1.3));
         break;
     case ABILITY_SHEER_FORCE:
         if (MoveIsAffectedBySheerForce(move))

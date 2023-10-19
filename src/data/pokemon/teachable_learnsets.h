d2e0656e5a36e2f26320ef5b867228e287d2e610
Frank <fdeblasio1004@gmail.com>
2023-10-19 17:34:05 -0400

    More buffs

--- src/data/pokemon/teachable_learnsets.h
+++ src/data/pokemon/teachable_learnsets.h
@@ -22851,6 +22851,7 @@ static const u16 sGranbullTeachableLearnset[] = {
     MOVE_PLAY_ROUGH,
     MOVE_PRESENT,
     MOVE_PROTECT,
+    MOVE_PSYCHIC_FANGS,
     MOVE_RAIN_DANCE,
     MOVE_REFLECT,
     MOVE_REST,

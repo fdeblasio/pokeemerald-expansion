4839ae3271630c5bc274cbcf0b5793c4664beea9
Frank <fdeblasio1004@gmail.com>
2023-11-06 14:16:11 -0500

    Set Gen 9 to false

--- include/config/pokemon.h
+++ include/config/pokemon.h
@@ -49,7 +49,7 @@
 #define P_GEN_6_POKEMON             TRUE        // Generation 6 Pokémon (XY, ORAS)
 #define P_GEN_7_POKEMON             TRUE        // Generation 7 Pokémon (SM, USUM, LGPE)
 #define P_GEN_8_POKEMON             TRUE        // Generation 8 Pokémon (SwSh, BDSP, LA)
-#define P_GEN_9_POKEMON             TRUE        // Generation 9 Pokémon (SV)
+#define P_GEN_9_POKEMON             FALSE       // Generation 9 Pokémon (SV)
 
 // Go here if you want to disable specific families of Pokémon.
 #include "config/species_families.h"

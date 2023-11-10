15dae04976afd3a984c9946cd61dd9c9c712558e
Frank <fdeblasio1004@gmail.com>
2023-11-10 11:14:04 -0500

    Switching Pokemon now stays on EVs/IVs if selected

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -1612,7 +1612,7 @@ static void CloseSummaryScreen(u8 taskId)
 }
 
 // Cycle summary page between stats and EVs/IVs
-static void ChangeSummaryState (s16 *taskData, u8 taskId)
+static void ChangeSummaryState(s16 *taskData, u8 taskId)
 {
     taskData[3] = (taskData[3] + 1) % 2;
     gTasks[taskId].func = Task_HandleInput;
@@ -1625,12 +1625,10 @@ static void Task_HandleInput(u8 taskId)
     {
         if (JOY_NEW(DPAD_UP))
         {
-            data[3] = 0;
             ChangeSummaryPokemon(taskId, -1);
         }
         else if (JOY_NEW(DPAD_DOWN))
         {
-            data[3] = 0;
             ChangeSummaryPokemon(taskId, 1);
         }
         else if ((JOY_NEW(DPAD_LEFT)) || GetLRKeysPressed() == MENU_L_PRESSED)
@@ -3397,10 +3395,7 @@ static void PrintSkillsPageText(void)
 {
     PrintHeldItemName();
     PrintRibbonCount();
-    BufferLeftColumnStats();
-    PrintLeftColumnStats();
-    BufferRightColumnStats();
-    PrintRightColumnStats();
+    BufferIvOrEvStats(gTasks[0].data[3]);
     PrintExpPointsNextLevel();
 }
 

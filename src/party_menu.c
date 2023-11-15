37b25bf552d06c5221cc8cd421c8d2e79599e401
Frank <fdeblasio1004@gmail.com>
2023-11-14 22:45:59 -0500

    Removed new instance of RemovePPBonus

--- src/party_menu.c
+++ src/party_menu.c
@@ -6114,7 +6114,6 @@ void DeleteMove(struct Pokemon *mon, u32 move)
             if (existingMove == move)
             {
                 SetMonMoveSlot(mon, MOVE_NONE, i);
-                RemoveMonPPBonus(mon, i);
                 for (j = i; j < MAX_MON_MOVES - 1; j++)
                     ShiftMoveSlot(mon, j, j + 1);
                 break;

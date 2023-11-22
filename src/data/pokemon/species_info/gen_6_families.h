16a3785ac0abf3eeb6276e251edea23418ac270d
Frank <fdeblasio1004@gmail.com>
2023-11-22 12:26:20 -0500

    Upped type name length to 7

--- src/data/pokemon/species_info/gen_6_families.h
+++ src/data/pokemon/species_info/gen_6_families.h
@@ -4555,7 +4555,6 @@ const struct SpeciesInfo gSpeciesInfoGen6[] =
         .eggMoveLearnset = sPumpkabooEggMoveLearnset,
         .formSpeciesIdTable = sPumpkabooFormSpeciesIdTable,
         .evolutions = EVOLUTION({EVO_ITEM, ITEM_LINKING_CORD, SPECIES_GOURGEIST_SMALL}),
-=======
 
     [SPECIES_PUMPKABOO_LARGE] =
     {

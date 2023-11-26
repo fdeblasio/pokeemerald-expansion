5fbbb1746f96047dfafba551ec440f75a4d84207
Frank <fdeblasio1004@gmail.com>
2023-11-26 12:11:38 -0500

    Added Fly to the Start Menu

--- src/strings.c
+++ src/strings.c
@@ -1521,6 +1521,7 @@ const u8 gText_UpdatedTime[] = _("Updated time"); // Unused
 const u8 gText_MenuPokedex[] = _("Pokédex");
 const u8 gText_MenuPokemon[] = _("Pokémon");
 const u8 gText_MenuBag[] = _("Bag");
+const u8 gText_MenuFly[] = _("Fly");
 const u8 gText_MenuPokenav[] = _("PokéNav");
 const u8 gText_MenuPlayer[] = _("{PLAYER}");
 const u8 gText_MenuSave[] = _("Save");

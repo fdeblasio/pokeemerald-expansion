96673415bb580dc19ca94c6b289632c111bbe5ae
Frank <fdeblasio1004@gmail.com>
2023-11-28 18:56:46 -0500

    Prevented Fly from appearing in start menu if it shouldn't

--- src/start_menu.c
+++ src/start_menu.c
@@ -351,7 +351,7 @@ static void BuildNormalStartMenu(void)
     AddStartMenuAction(MENU_ACTION_SAVE);
     AddStartMenuAction(MENU_ACTION_OPTION);
 
-    if (FlagGet(FLAG_BADGE03_GET) == TRUE)
+    if (FlagGet(FLAG_BADGE03_GET) == TRUE && Overworld_MapTypeAllowsTeleportAndFly(gMapHeader.mapType) == TRUE)
     {
         AddStartMenuAction(MENU_ACTION_FLY);
     }
@@ -732,7 +732,7 @@ static bool8 StartMenuFlyCallback(void)
         PlayRainStoppingSoundEffect();
         RemoveExtraStartMenuWindows();
         CleanupOverworldWindowsAndTilemaps();
-        SetMainCallback2(CB2_OpenFlyMap); // Display bag menu
+        SetMainCallback2(CB2_OpenFlyMap); // Display Fly map
 
         return TRUE;
     }

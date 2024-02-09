66a46534f585b53cefc4df4413a3347ecc3bb366
Frank <fdeblasio1004@gmail.com>
2024-02-09 14:16:19 -0500

    Updated level up moves for Indigo Disk and to use in-game order

--- include/config/pokemon.h
+++ include/config/pokemon.h
@@ -52,7 +52,7 @@
 #define P_NO_SHINIES_WITHOUT_POKEBALLS   TRUE       // If TRUE, Pokémon encountered when the player is out of Poké Balls won't be shiny
 
 // Learnset helper toggles
-#define P_LEARNSET_HELPER_TEACHABLE TRUE        // If TRUE, teachable_learnsets.h will be populated by tools/learnset_helpers/teachable.py using the included JSON files based on available TMs and tutors.
+#define P_LEARNSET_HELPER_TEACHABLE FALSE       // If TRUE, teachable_learnsets.h will be populated by tools/learnset_helpers/teachable.py using the included JSON files based on available TMs and tutors.
 
 // Flag settings
 // To use the following features in scripting, replace the 0s with the flag ID you're assigning it to.

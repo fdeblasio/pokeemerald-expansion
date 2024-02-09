ba95b18e6ac9eff5e898af986b4f32db48d8289e
Frank <fdeblasio1004@gmail.com>
2024-02-09 11:50:31 -0500

    Re-updated movesets after rebase

--- src/pokemon.c
+++ src/pokemon.c
@@ -685,7 +685,7 @@ const struct NatureInfo gNaturesInfo[NUM_NATURES] =
 #include "data/pokemon/experience_tables.h"
 
 #if P_LVL_UP_LEARNSETS >= GEN_9
-#include "data/pokemon/level_up_learnsets/gen_9.h" // Scarlet/Violet
+#include "data/pokemon/level_up_learnsets/custom.h"
 #elif P_LVL_UP_LEARNSETS >= GEN_8
 #include "data/pokemon/level_up_learnsets/gen_8.h" // Sword/Shield
 #elif P_LVL_UP_LEARNSETS >= GEN_7

8d741a98908c9109d49a3cd113d57341f1f9298b
Frank <fdeblasio1004@gmail.com>
2024-06-04 18:12:30 -0400

    Remove seemingly obsolete OW sprite value

--- src/data/object_events/object_event_graphics_info.h
+++ src/data/object_events/object_event_graphics_info.h
@@ -4663,7 +4663,6 @@ const struct ObjectEventGraphicsInfo gObjectEventGraphicsInfo_Frank = {
     .paletteSlot = PALSLOT_NPC_SPECIAL,
     .shadowSize = SHADOW_SIZE_M,
     .inanimate = FALSE,
-    .disableReflectionPaletteLoad = FALSE,
     .tracks = TRACKS_FOOT,
     .oam = &gObjectEventBaseOam_16x32,
     .subspriteTables = sOamTables_16x32,

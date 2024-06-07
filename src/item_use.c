18e08de17b0f9a7e7070f11b967f71c711157775
Frank <fdeblasio1004@gmail.com>
2024-06-07 00:28:33 -0400

    Decapped last ball thrown

--- src/item_use.c
+++ src/item_use.c
@@ -1141,7 +1141,7 @@ bool32 CanThrowBall(void)
 
 static const u8 sText_CantThrowPokeBall_TwoMons[] = _("Cannot throw a ball!\nThere are two Pokémon out there!\p");
 static const u8 sText_CantThrowPokeBall_SemiInvulnerable[] = _("Cannot throw a ball!\nThere's no Pokémon in sight!\p");
-static const u8 sText_CantThrowPokeBall_Disabled[] = _("POKé BALLS cannot be used\nright now!\p");
+static const u8 sText_CantThrowPokeBall_Disabled[] = _("Poké Balls cannot be used\nright now!\p");
 
 static void ItemUseInBattle_ShowPartyMenu(u8 taskId)
 {

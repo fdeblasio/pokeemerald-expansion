8095d57e1d9f73365b7d3688dc36581a2ec46b64
Frank <fdeblasio1004@gmail.com>
2024-06-26 14:35:57 -0400

    Updated accuracy on status screens

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -3982,6 +3982,9 @@ static void PrintMoveNameAndPP(u8 moveIndex)
 static void PrintMovePowerAndAccuracy(u16 moveIndex)
 {
     const u8 *text;
+    struct Pokemon *mon = &sMonSummaryScreen->currentMon;
+    u8 monFriendship = GetMonData(mon, MON_DATA_FRIENDSHIP);
+
     if (moveIndex != MOVE_NONE)
     {
         FillWindowPixelRect(PSS_LABEL_WINDOW_MOVES_POWER_ACC, PIXEL_FILL(0), 53, 0, 19, 32);
@@ -4010,10 +4013,21 @@ static void PrintMovePowerAndAccuracy(u16 moveIndex)
         PrintTextOnWindow(PSS_LABEL_WINDOW_MOVES_POWER_ACC, text, 53, 1, 0, 0);
 
         u32 accuracy = GetMoveAccuracy(moveIndex);
+        if (GetMonAbility(mon) == ABILITY_COMPOUND_EYES)
+            accuracy = (accuracy * 130) / 100;
+        else if (GetMonAbility(mon) == ABILITY_VICTORY_STAR)
+            accuracy = (accuracy * 110) / 100;
+        else if (GetMonAbility(mon) == ABILITY_HUSTLE && gMovesInfo[moveIndex].category == DAMAGE_CATEGORY_PHYSICAL)
+            accuracy = (accuracy * 80) / 100;
+
+        if (GetMonData(mon, MON_DATA_HELD_ITEM) == ITEM_WIDE_LENS)
+            accuracy = (accuracy * 110) / 100;
+
+        if (accuracy > 100)
+            accuracy = 100;
+
         if (accuracy == 0)
-        {
             text = gText_ThreeDashes;
-        }
         else
         {
             ConvertIntToDecimalStringN(gStringVar1, accuracy, STR_CONV_MODE_RIGHT_ALIGN, 3);

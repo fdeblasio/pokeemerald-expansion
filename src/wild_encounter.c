ca4789551cd566bc5af939d7100e5a8588c1f8f8
Frank <fdeblasio1004@gmail.com>
2024-07-02 16:27:18 -0400

    Updated Route 114

--- src/wild_encounter.c
+++ src/wild_encounter.c
@@ -82,6 +82,9 @@ EWRAM_DATA u8 gChainFishingDexNavStreak = 0;
 #define RANGE_GOOD_ROD_INITIAL  22, 25
 #define RANGE_ROUTE_112         23, 24
 #define RANGE_FIERY_PATH        24, 25
+#define RANGE_ROUTE_113         24, 25
+#define RANGE_ROUTE_114         24, 25
+#define RANGE_METEOR_FALLS      24, 25
 #define RANGE_ROUTE_119         42, 44
 
 #include "data/wild_encounters.h"

aa9c6686d85dd8632f6b734809fd4c59a7adbb61
Frank <fdeblasio1004@gmail.com>
2024-07-19 13:29:24 -0400

    Revert "Removed references to HMs"
    This reverts commit 7f88e8da442db250a74a31f987beeb1ec0aab92e.

--- src/evolution_scene.c
+++ src/evolution_scene.c
@@ -982,6 +982,17 @@ static void Task_EvolutionScene(u8 taskId)
                 {
                     // Selected move to forget
                     u16 move = GetMonData(mon, var + MON_DATA_MOVE1);
+                    if (IsMoveHM(move))
+                    {
+                        // Can't forget HMs
+                        BattleStringExpandPlaceholdersToDisplayedString(gBattleStringsTable[STRINGID_HMMOVESCANTBEFORGOTTEN]);
+                        BattlePutTextOnWindow(gDisplayedStringBattle, B_WIN_MSG);
+                        gTasks[taskId].tLearnMoveState = MVSTATE_RETRY_AFTER_HM;
+                    }
+                    else
+                    {
+                        // Forget move
+                        PREPARE_MOVE_BUFFER(gBattleTextBuff2, move)
 
                     // Forget move
                     PREPARE_MOVE_BUFFER(gBattleTextBuff2, move)
@@ -1351,7 +1362,6 @@ static void Task_TradeEvolutionScene(u8 taskId)
                 {
                     // Selected move to forget
                     u16 move = GetMonData(mon, var + MON_DATA_MOVE1);
-<<<<<<< HEAD
                     if (IsMoveHM(move))
                     {
                         // Can't forget HMs
@@ -1369,16 +1379,6 @@ static void Task_TradeEvolutionScene(u8 taskId)
                         DrawTextOnTradeWindow(0, gDisplayedStringBattle, 1);
                         gTasks[taskId].tLearnMoveState++;
                     }
-=======
-
-                    // Forget move
-                    PREPARE_MOVE_BUFFER(gBattleTextBuff2, move)
-
-                    SetMonMoveSlot(mon, gMoveToLearn, var);
-                    BattleStringExpandPlaceholdersToDisplayedString(gBattleStringsTable[STRINGID_123POOF - BATTLESTRINGS_TABLE_START]);
-                    DrawTextOnTradeWindow(0, gDisplayedStringBattle, 1);
-                    gTasks[taskId].tLearnMoveState++;
->>>>>>> 4ef3638e95 (Removed references to HMs)
                 }
             }
             break;

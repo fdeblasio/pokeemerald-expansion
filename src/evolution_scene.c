3339bd607fa2b7f47f848d572e5d28e9e20ddaf6
Frank <fdeblasio1004@gmail.com>
2024-07-19 08:26:59 -0400

    Removed references to HMs

--- src/evolution_scene.c
+++ src/evolution_scene.c
@@ -630,7 +630,6 @@ enum {
     MVSTATE_LEARNED_MOVE,
     MVSTATE_ASK_CANCEL,
     MVSTATE_CANCEL,
-    MVSTATE_RETRY_AFTER_HM,
 };
 
 // Task data from CycleEvolutionMonSprite
@@ -983,21 +982,12 @@ static void Task_EvolutionScene(u8 taskId)
                 {
                     // Selected move to forget
                     u16 move = GetMonData(mon, var + MON_DATA_MOVE1);
-                    if (IsMoveHM(move))
-                    {
-                        // Can't forget HMs
-                        BattleStringExpandPlaceholdersToDisplayedString(gBattleStringsTable[STRINGID_HMMOVESCANTBEFORGOTTEN]);
-                        BattlePutTextOnWindow(gDisplayedStringBattle, B_WIN_MSG);
-                        gTasks[taskId].tLearnMoveState = MVSTATE_RETRY_AFTER_HM;
-                    }
-                    else
-                    {
-                        // Forget move
-                        PREPARE_MOVE_BUFFER(gBattleTextBuff2, move)
 
-                        SetMonMoveSlot(mon, gMoveToLearn, var);
-                        gTasks[taskId].tLearnMoveState++;
-                    }
+                    // Forget move
+                    PREPARE_MOVE_BUFFER(gBattleTextBuff2, move)
+
+                    SetMonMoveSlot(mon, gMoveToLearn, var);
+                    gTasks[taskId].tLearnMoveState++;
                 }
             }
             break;
@@ -1033,10 +1023,6 @@ static void Task_EvolutionScene(u8 taskId)
             BattleStringExpandPlaceholdersToDisplayedString(gBattleStringsTable[STRINGID_DIDNOTLEARNMOVE]);
             BattlePutTextOnWindow(gDisplayedStringBattle, B_WIN_MSG);
             gTasks[taskId].tState = EVOSTATE_TRY_LEARN_MOVE;
-            break;
-        case MVSTATE_RETRY_AFTER_HM:
-            if (!IsTextPrinterActive(0) && !IsSEPlaying())
-                gTasks[taskId].tLearnMoveState = MVSTATE_SHOW_MOVE_SELECT;
             break;
         }
         break;
@@ -1081,7 +1067,6 @@ enum {
     T_MVSTATE_LEARNED_MOVE,
     T_MVSTATE_ASK_CANCEL,
     T_MVSTATE_CANCEL,
-    T_MVSTATE_RETRY_AFTER_HM,
 };
 
 // Compare to Task_EvolutionScene, very similar
@@ -1366,6 +1351,7 @@ static void Task_TradeEvolutionScene(u8 taskId)
                 {
                     // Selected move to forget
                     u16 move = GetMonData(mon, var + MON_DATA_MOVE1);
+<<<<<<< HEAD
                     if (IsMoveHM(move))
                     {
                         // Can't forget HMs
@@ -1383,6 +1369,16 @@ static void Task_TradeEvolutionScene(u8 taskId)
                         DrawTextOnTradeWindow(0, gDisplayedStringBattle, 1);
                         gTasks[taskId].tLearnMoveState++;
                     }
+=======
+
+                    // Forget move
+                    PREPARE_MOVE_BUFFER(gBattleTextBuff2, move)
+
+                    SetMonMoveSlot(mon, gMoveToLearn, var);
+                    BattleStringExpandPlaceholdersToDisplayedString(gBattleStringsTable[STRINGID_123POOF - BATTLESTRINGS_TABLE_START]);
+                    DrawTextOnTradeWindow(0, gDisplayedStringBattle, 1);
+                    gTasks[taskId].tLearnMoveState++;
+>>>>>>> 4ef3638e95 (Removed references to HMs)
                 }
             }
             break;
@@ -1413,10 +1409,6 @@ static void Task_TradeEvolutionScene(u8 taskId)
             BattleStringExpandPlaceholdersToDisplayedString(gBattleStringsTable[STRINGID_DIDNOTLEARNMOVE]);
             DrawTextOnTradeWindow(0, gDisplayedStringBattle, 1);
             gTasks[taskId].tState = T_EVOSTATE_TRY_LEARN_MOVE;
-            break;
-        case T_MVSTATE_RETRY_AFTER_HM:
-            if (!IsTextPrinterActive(0) && !IsSEPlaying())
-                gTasks[taskId].tLearnMoveState = T_MVSTATE_SHOW_MOVE_SELECT;
             break;
         }
         break;

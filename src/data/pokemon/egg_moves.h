fe8f0be70a4fb3900165146856c661ebf11935f3
Frank <fdeblasio1004@gmail.com>
2024-07-27 10:30:00 -0400

    Added Pachi evo

--- src/data/pokemon/egg_moves.h
+++ src/data/pokemon/egg_moves.h
@@ -6519,6 +6519,7 @@ static const u16 sPachirisuEggMoveLearnset[] = {
     MOVE_MUD_SLAP,
     MOVE_ROUND,
     MOVE_SNORE,
+    MOVE_TAIL_SLAP,
     MOVE_TAIL_WHIP,
     MOVE_TAKE_DOWN,
     MOVE_TERA_BLAST,

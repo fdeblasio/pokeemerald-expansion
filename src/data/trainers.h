9027ef764cf040476935411200fd426ba134eaad
Frank <fdeblasio1004@gmail.com>
2024-07-27 08:58:00 -0400

    Updated Pokedex for Petalburg Woods

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -5069,9 +5069,9 @@ WALLACE_BATTLE(WALLACE_REMATCH, 90),
     .lvl = Level,             \
     .species = SPECIES_SHEDINJA
 
-#define JAMES_DUSTOX(Level)    \
-    .lvl = Level,              \
-    .species = SPECIES_DUSTOX, \
+#define JAMES_BUTTERFREE(Level)    \
+    .lvl = Level,                  \
+    .species = SPECIES_BUTTERFREE, \
     .gender = TRAINER_MON_MALE
 
 #define JAMES_SURSKIT(Level)                                                     \
@@ -5116,7 +5116,7 @@ WALLACE_BATTLE(WALLACE_REMATCH, 90),
     .partySize = 3,
     .party = (const struct TrainerMon[]) {
         {
-        JAMES_DUSTOX(REMATCH_3_LEVEL_3),
+        JAMES_BUTTERFREE(REMATCH_3_LEVEL_3),
         IVS(3),
         },
         {
@@ -5140,7 +5140,7 @@ WALLACE_BATTLE(WALLACE_REMATCH, 90),
         IVS(4),
         },
         {
-        JAMES_DUSTOX(REMATCH_4_LEVEL_4),
+        JAMES_BUTTERFREE(REMATCH_4_LEVEL_4),
         IVS(4),
         },
         {
@@ -5165,12 +5165,12 @@ WALLACE_BATTLE(WALLACE_REMATCH, 90),
         },
         {
         .lvl = REMATCH_5_LEVEL_5,
-        .species = SPECIES_BEAUTIFLY,
+        .species = SPECIES_ESCAVALIER,
         IVS(5),
-        .gender = TRAINER_MON_FEMALE,
+        .gender = TRAINER_MON_MALE,
         },
         {
-        JAMES_DUSTOX(REMATCH_5_LEVEL_5),
+        JAMES_BUTTERFREE(REMATCH_5_LEVEL_4),
         IVS(5),
         },
         {

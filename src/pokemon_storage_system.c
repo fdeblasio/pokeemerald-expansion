67a468a4931747a199d69f6166e28b853917995a
Frank <fdeblasio1004@gmail.com>
2025-01-21 17:44:38 -0500

    Decapped new Pokemon strings

--- src/pokemon_storage_system.c
+++ src/pokemon_storage_system.c
@@ -858,7 +858,7 @@ void SetMonFormPSS(struct BoxPokemon *boxMon, enum FormChanges method);
 void SetMonFormPSS_ItemHold(struct BoxPokemon *boxMon);
 void UpdateSpeciesSpritePSS(struct BoxPokemon *boxmon);
 
-static const u8 gText_JustOnePkmn[] = _("There is just one POKéMON with you.");
+static const u8 gText_JustOnePkmn[] = _("There is just one Pokémon with you.");
 static const u8 gText_PartyFull[] = _("Your party is full!");
 static const u8 gText_Box[] = _("BOX");
 

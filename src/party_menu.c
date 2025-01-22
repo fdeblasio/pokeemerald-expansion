d5d8f4cba73f1fe5bdcad7c469bb7f3a72746949
Frank <fdeblasio1004@gmail.com>
2025-01-22 11:26:37 -0500

    Updated TMHM variable names

--- src/party_menu.c
+++ src/party_menu.c
@@ -5448,7 +5448,7 @@ static void Task_LearnedMove(u8 taskId)
 
     if (move[1] == 0)
     {
-        AdjustFriendship(mon, FRIENDSHIP_EVENT_LEARN_TMHM);
+        AdjustFriendship(mon, FRIENDSHIP_EVENT_LEARN_TM);
         if (!GetItemImportance(item))
             RemoveBagItem(item, 1);
     }

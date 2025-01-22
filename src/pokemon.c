0d973e4fa6cced4be25b5db225534469d870d68d
Frank <fdeblasio1004@gmail.com>
2025-01-22 11:10:05 -0500

    Pokedex and minor updates

--- src/pokemon.c
+++ src/pokemon.c
@@ -135,9 +135,6 @@ static const enum NationalDexOrder sHoennToNationalOrder[HOENN_DEX_COUNT - 1] =
     HOENN_TO_NATIONAL(MIGHTYENA),
     HOENN_TO_NATIONAL(ZIGZAGOON),
     HOENN_TO_NATIONAL(LINOONE),
-#if P_NEW_EVOS_IN_REGIONAL_DEX && P_GALARIAN_FORMS
-    HOENN_TO_NATIONAL(OBSTAGOON),
-#endif
     HOENN_TO_NATIONAL(WURMPLE),
     HOENN_TO_NATIONAL(SILCOON),
     HOENN_TO_NATIONAL(BEAUTIFLY),
@@ -249,6 +246,8 @@ static const enum NationalDexOrder sHoennToNationalOrder[HOENN_DEX_COUNT - 1] =
     HOENN_TO_NATIONAL(MUK),
     HOENN_TO_NATIONAL(KOFFING),
     HOENN_TO_NATIONAL(WEEZING),
+    HOENN_TO_NATIONAL(HOUNDOUR),
+    HOENN_TO_NATIONAL(HOUNDOOM),
     HOENN_TO_NATIONAL(SPOINK),
     HOENN_TO_NATIONAL(GRUMPIG),
     HOENN_TO_NATIONAL(SANDSHREW),
@@ -335,9 +334,6 @@ static const enum NationalDexOrder sHoennToNationalOrder[HOENN_DEX_COUNT - 1] =
     HOENN_TO_NATIONAL(GOREBYSS),
     HOENN_TO_NATIONAL(RELICANTH),
     HOENN_TO_NATIONAL(CORSOLA),
-#if P_NEW_EVOS_IN_REGIONAL_DEX && P_GALARIAN_FORMS
-    HOENN_TO_NATIONAL(CURSOLA),
-#endif
     HOENN_TO_NATIONAL(CHINCHOU),
     HOENN_TO_NATIONAL(LANTURN),
     HOENN_TO_NATIONAL(LUVDISC),
@@ -687,7 +683,9 @@ const struct NatureInfo gNaturesInfo[NUM_NATURES] =
 #include "data/pokemon/trainer_class_lookups.h"
 #include "data/pokemon/experience_tables.h"
 
-#if P_LVL_UP_LEARNSETS >= GEN_9
+#if P_LVL_UP_LEARNSETS == GEN_LATEST
+#include "data/pokemon/level_up_learnsets/custom.h"
+#elif P_LVL_UP_LEARNSETS >= GEN_9
 #include "data/pokemon/level_up_learnsets/gen_9.h" // Scarlet/Violet
 #elif P_LVL_UP_LEARNSETS >= GEN_8
 #include "data/pokemon/level_up_learnsets/gen_8.h" // Sword/Shield

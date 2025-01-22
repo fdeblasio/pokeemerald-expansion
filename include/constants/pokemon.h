2dacbd0f7c6d8741e99fc8772000cb798856ed95
Frank <fdeblasio1004@gmail.com>
2025-01-22 11:47:11 -0500

    Updates to various percentages and values

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -208,7 +208,7 @@ enum __attribute__((packed)) Stat
 #define AFFECTION_FIVE_HEARTS   5 // Max friendship
 
 // Friendship value that the majority of species use.
-#define STANDARD_FRIENDSHIP ((P_UPDATED_FRIENDSHIP >= GEN_8) ? 50 : 70)
+#define STANDARD_FRIENDSHIP ((P_UPDATED_FRIENDSHIP >= GEN_8) ? 70 : 70)
 
 #define MAX_FRIENDSHIP  255
 #define MAX_SHEEN       255
@@ -217,8 +217,8 @@ enum __attribute__((packed)) Stat
 #define MAX_PER_STAT_IVS 31
 #define MAX_IV_MASK 31
 #define USE_RANDOM_IVS (MAX_PER_STAT_IVS + 1)
-#define MAX_PER_STAT_EVS ((P_EV_CAP >= GEN_6) ? 252 : 255)
-#define MAX_TOTAL_EVS 510
+#define MAX_PER_STAT_EVS 63
+#define MAX_TOTAL_EVS 127
 #define EV_REMAINDER (MAX_TOTAL_EVS - (2 * MAX_PER_STAT_EVS))
 #define EV_SPLIT_THREE (MAX_TOTAL_EVS/3)
 #define EV_SPLIT_FOUR  (MAX_TOTAL_EVS/4)

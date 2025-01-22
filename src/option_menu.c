4d4b760d430b29742d840c3605468298d6712891
Frank <fdeblasio1004@gmail.com>
2025-01-22 11:47:11 -0500

    Updates to various percentages and values

--- src/option_menu.c
+++ src/option_menu.c
@@ -132,7 +132,7 @@ static const struct BgTemplate sOptionMenuBgTemplates[] =
     }
 };
 
-static const u16 sOptionMenuBg_Pal[] = {RGB(17, 18, 31)};
+static const u16 sOptionMenuBg_Pal[] = {RGB(31, 31, 0)};
 
 static void MainCB2(void)
 {

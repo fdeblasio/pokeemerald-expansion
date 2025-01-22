e62cd6f57f25423c873d80768efd095dc6d2d714
Frank <fdeblasio1004@gmail.com>
2025-01-22 13:21:11 -0500

    Updated Mirage Island

--- src/time_events.c
+++ src/time_events.c
@@ -9,46 +9,9 @@
 #include "script.h"
 #include "task.h"
 
-static u32 GetMirageRnd(void)
-{
-    u32 hi = VarGet(VAR_MIRAGE_RND_H);
-    u32 lo = VarGet(VAR_MIRAGE_RND_L);
-    return (hi << 16) | lo;
-}
-
-static void SetMirageRnd(u32 rnd)
-{
-    VarSet(VAR_MIRAGE_RND_H, rnd >> 16);
-    VarSet(VAR_MIRAGE_RND_L, rnd);
-}
-
-// unused
-void InitMirageRnd(void)
-{
-    SetMirageRnd(Random32());
-}
-
-void UpdateMirageRnd(u16 days)
-{
-    s32 rnd = GetMirageRnd();
-    while (days)
-    {
-        rnd = ISO_RANDOMIZE2(rnd);
-        days--;
-    }
-    SetMirageRnd(rnd);
-}
-
 bool8 IsMirageIslandPresent(void)
 {
-    u16 rnd = GetMirageRnd() >> 16;
-    int i;
-
-    for (i = 0; i < PARTY_SIZE; i++)
-        if (GetMonData(&gPlayerParty[i], MON_DATA_SPECIES) && (GetMonData(&gPlayerParty[i], MON_DATA_PERSONALITY) & 0xFFFF) == rnd)
-            return TRUE;
-
-    return FALSE;
+    return TRUE;
 }
 
 void UpdateShoalTideFlag(void)
@@ -58,27 +21,27 @@ void UpdateShoalTideFlag(void)
         1, // 00
         1, // 01
         1, // 02
-        0, // 03
-        0, // 04
-        0, // 05
+        1, // 03
+        1, // 04
+        1, // 05
         0, // 06
         0, // 07
         0, // 08
-        1, // 09
-        1, // 10
-        1, // 11
-        1, // 12
-        1, // 13
-        1, // 14
+        0, // 09
+        0, // 10
+        0, // 11
+        0, // 12
+        0, // 13
+        0, // 14
         0, // 15
         0, // 16
         0, // 17
         0, // 18
         0, // 19
         0, // 20
-        1, // 21
-        1, // 22
-        1, // 23
+        0, // 21
+        0, // 22
+        0, // 23
     };
 
     if (IsMapTypeOutdoors(GetLastUsedWarpMapType()))

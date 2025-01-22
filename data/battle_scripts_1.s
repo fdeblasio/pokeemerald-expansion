42849a9159b6ad8af7f71f00f4d2f12fc14b4464
Frank <fdeblasio1004@gmail.com>
2025-01-22 17:05:08 -0500

    Updated Present

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -3553,6 +3553,7 @@ BattleScript_EffectAttract::
 	goto BattleScript_MoveEnd
 
 BattleScript_EffectPresent::
+	jumpiftargetally BattleScript_EffectHealPulse
 	attackcanceler
 	accuracycheck BattleScript_MoveMissedPause, ACC_CURR_MOVE
 	typecalc

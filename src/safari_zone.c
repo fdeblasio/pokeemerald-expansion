690acc9c7ca783d7debfc375d88fd95855fa9a3d
Frank <fdeblasio1004@gmail.com>
2025-01-22 11:47:11 -0500

    Updates to various percentages and values

--- src/safari_zone.c
+++ src/safari_zone.c
@@ -57,11 +57,8 @@ void EnterSafariMode(void)
     IncrementGameStat(GAME_STAT_ENTERED_SAFARI_ZONE);
     SetSafariZoneFlag();
     ClearAllPokeblockFeeders();
-    gNumSafariBalls = 30;
-    if (IS_FRLG)
-        gSafariZoneStepCounter = 600;
-    else
-        gSafariZoneStepCounter = 500;
+    gNumSafariBalls = 50;
+    gSafariZoneStepCounter = 1000;
     sSafariZoneCaughtMons = 0;
     sSafariZonePkblkUses = 0;
 }

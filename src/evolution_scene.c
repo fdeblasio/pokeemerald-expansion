a22ca7168a865daab8ecdf7aed97a086de5eaf7d
Frank <fdeblasio1004@gmail.com>
2025-01-22 20:30:03 -0500

    Improved learning moves

--- src/evolution_scene.c
+++ src/evolution_scene.c
@@ -1009,10 +1009,13 @@ static void Task_EvolutionScene(u8 taskId)
                     {
                         // Forget move
                         PREPARE_MOVE_BUFFER(gBattleTextBuff2, move)
-
-                        SetMonMoveSlot(mon, gMoveToLearn, var);
-                        gTasks[taskId].tLearnMoveState++;
                     }
+
+                    // Forget move
+                    PREPARE_MOVE_BUFFER(gBattleTextBuff2, move)
+
+                    SetMonMoveSlot(mon, gMoveToLearn, var);
+                    gTasks[taskId].tLearnMoveState++;
                 }
             }
             break;

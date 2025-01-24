ea4ebde5f17386eea9e51c38ee36f9047586d494
Frank <fdeblasio1004@gmail.com>
2025-01-23 20:01:02 -0500

    Added dynamic accuracy

--- include/battle_main.h
+++ include/battle_main.h
@@ -82,6 +82,7 @@ void RunBattleScriptCommands(void);
 void SpecialStatusesClear(void);
 u32 GetDynamicMoveType(struct Pokemon *mon, u32 move, u32 battler, u8 *ateBoost);
 void SetTypeBeforeUsingMove(u32 move, u32 battlerAtk);
+u32 GetDynamicAccuracy(struct Pokemon *mon, u32 move, u32 battler);
 bool32 IsWildMonSmart(void);
 u8 CreateNPCTrainerPartyFromTrainer(struct Pokemon *party, const struct Trainer *trainer, bool32 firstTrainer, u32 battleTypeFlags);
 void ModifyPersonalityForNature(u32 *personality, u32 newNature);

93e6b89ed3f6a1e5a5e7f04b5726a78d683e11a8
Frank <fdeblasio1004@gmail.com>
2025-01-25 14:46:46 -0500

    Gave Wind Power immunity to wind moves

--- src/battle_util.c
+++ src/battle_util.c
@@ -3293,6 +3293,10 @@ bool32 CanAbilityAbsorbMove(u32 battlerAtk, u32 battlerDef, u32 abilityDef, u32 
             statId = STAT_ATK;
         }
         break;
+    case ABILITY_WIND_POWER:
+        if (IsWindMove(move) && !(GetBattlerMoveTargetType(battlerAtk, move) & MOVE_TARGET_USER))
+            effect = MOVE_ABSORBED_BY_CHARGING;
+        break;
     case ABILITY_FLASH_FIRE:
         if (moveType == TYPE_FIRE && (B_FLASH_FIRE_FROZEN >= GEN_5 || !(gBattleMons[battlerDef].status1 & STATUS1_FREEZE)))
             effect = MOVE_ABSORBED_BY_BOOST_FLASH_FIRE;
@@ -3347,6 +3351,15 @@ bool32 CanAbilityAbsorbMove(u32 battlerAtk, u32 battlerDef, u32 abilityDef, u32 
         {
             gBattleCommunication[MULTISTRING_CHOOSER] = B_MSG_FLASH_FIRE_NO_BOOST;
             battleScript = BattleScript_FlashFireBoost;
+        }
+        break;
+    case MOVE_ABSORBED_BY_CHARGING:
+        gBattleStruct->pledgeMove = FALSE;
+        if (MoveResultHasEffect(gBattlerTarget)
+            && !gProtectStructs[battlerAtk].confusionSelfDmg
+            && IsBattlerAlive(gBattlerTarget))
+        {
+            battleScript = BattleScript_WindPowerActivates;
         }
         break;
     }
@@ -5037,16 +5050,12 @@ u32 AbilityBattleEffects(u32 caseID, u32 battler, u32 ability, u32 special, u32 
                 effect++;
             }
             break;
-        case ABILITY_WIND_POWER:
-            if (!IsWindMove(gCurrentMove))
-                break;
-            // fall through
         case ABILITY_ELECTROMORPHOSIS:
             if (!gProtectStructs[gBattlerAttacker].confusionSelfDmg
              && IsBattlerTurnDamaged(gBattlerTarget)
              && IsBattlerAlive(gBattlerTarget))
             {
-                BattleScriptCall(BattleScript_WindPowerActivates);
+                BattleScriptCall(BattleScript_ElectromorphosisActivates);
                 effect++;
             }
             break;

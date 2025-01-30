4b6d93d24f3b671ed6afcdbb2c46152179872a5c
Frank <fdeblasio1004@gmail.com>
2025-01-30 17:12:53 -0500

    Cleaned up placeholders

--- src/string_util.c
+++ src/string_util.c
@@ -490,39 +490,10 @@ static const u8 *ExpandPlaceholder_RivalName(void)
         return (IS_FRLG ? gText_ExpandedPlaceholder_Red : gText_ExpandedPlaceholder_Brendan);
 }
 
-static const u8 *ExpandPlaceholder_Version(void)
+const u8 gText_ExpandedPlaceholder_Wally[] = _("Wally");
+static const u8 *ExpandPlaceholder_Wally(void)
 {
-    return gText_ExpandedPlaceholder_Emerald;
-}
-
-static const u8 *ExpandPlaceholder_Aqua(void)
-{
-    return gText_ExpandedPlaceholder_Aqua;
-}
-
-static const u8 *ExpandPlaceholder_Magma(void)
-{
-    return gText_ExpandedPlaceholder_Magma;
-}
-
-static const u8 *ExpandPlaceholder_Archie(void)
-{
-    return gText_ExpandedPlaceholder_Archie;
-}
-
-static const u8 *ExpandPlaceholder_Maxie(void)
-{
-    return gText_ExpandedPlaceholder_Maxie;
-}
-
-static const u8 *ExpandPlaceholder_Kyogre(void)
-{
-    return gText_ExpandedPlaceholder_Kyogre;
-}
-
-static const u8 *ExpandPlaceholder_Groudon(void)
-{
-    return gText_ExpandedPlaceholder_Groudon;
+    return gText_ExpandedPlaceholder_Wally;
 }
 
 static const u8 *ExpandPlaceholder_Region(void)
@@ -531,12 +502,6 @@ static const u8 *ExpandPlaceholder_Region(void)
         return gText_Kanto;
     else
         return gText_Hoenn;
-}
-
-const u8 gText_ExpandedPlaceholder_Wally[] = _("Wally");
-static const u8 *ExpandPlaceholder_Wally(void)
-{
-    return gText_ExpandedPlaceholder_Wally;
 }
 
 const u8 *GetExpandedPlaceholder(u32 id)
@@ -552,15 +517,8 @@ const u8 *GetExpandedPlaceholder(u32 id)
         [PLACEHOLDER_ID_STRING_VAR_3] = ExpandPlaceholder_StringVar3,
         [PLACEHOLDER_ID_KUN]          = ExpandPlaceholder_KunChan,
         [PLACEHOLDER_ID_RIVAL]        = ExpandPlaceholder_RivalName,
-        [PLACEHOLDER_ID_VERSION]      = ExpandPlaceholder_Version,
-        [PLACEHOLDER_ID_AQUA]         = ExpandPlaceholder_Aqua,
-        [PLACEHOLDER_ID_MAGMA]        = ExpandPlaceholder_Magma,
-        [PLACEHOLDER_ID_ARCHIE]       = ExpandPlaceholder_Archie,
-        [PLACEHOLDER_ID_MAXIE]        = ExpandPlaceholder_Maxie,
-        [PLACEHOLDER_ID_KYOGRE]       = ExpandPlaceholder_Kyogre,
-        [PLACEHOLDER_ID_GROUDON]      = ExpandPlaceholder_Groudon,
-        [PLACEHOLDER_ID_REGION]       = ExpandPlaceholder_Region,
         [PLACEHOLDER_ID_WALLY]        = ExpandPlaceholder_Wally,
+        [PLACEHOLDER_ID_REGION]       = ExpandPlaceholder_Region,
     };
 
     if (id >= ARRAY_COUNT(funcs))

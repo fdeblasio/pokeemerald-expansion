17499b4234409bd72344add8154fa9960bcb12c3
Frank <fdeblasio1004@gmail.com>
2025-02-07 15:57:00 -0500

    Corrected met location for Mirage Island

--- src/pokemon.c
+++ src/pokemon.c
@@ -1216,6 +1216,8 @@ void CreateBoxMon(struct BoxPokemon *boxMon, u16 species, u8 level, u8 fixedIV, 
     if (species == SPECIES_MIRAGE_ISLAND && GetCurrentRegionMapSectionId() == MAPSEC_ROUTE_130){
         value = 2;
         SetBoxMonData(boxMon, MON_DATA_ABILITY_NUM, &value);
+        value = MAPSEC_MIRAGE_ISLAND;
+        SetBoxMonData(boxMon, MON_DATA_MET_LOCATION, &value);
     }
     else if (GetSpeciesAbility(species, 1))
     {

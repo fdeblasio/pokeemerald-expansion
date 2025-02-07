c45682f167cf93d3ca31ba58067a3359ba23ed2e
Frank <fdeblasio1004@gmail.com>
2025-02-07 18:42:58 -0500

    Updated Hitmons, Greninja, Infernape, Talonflame, Staraptor, and Venusaur

--- src/data/pokemon/teachable_learnsets.h
+++ src/data/pokemon/teachable_learnsets.h
@@ -22340,6 +22340,7 @@ static const u16 sBibarelTeachableLearnset[] = {
     MOVE_AQUA_TAIL,
     MOVE_ATTRACT,
     MOVE_BLIZZARD,
+    MOVE_BODY_SLAM,
     MOVE_CRUNCH,
     MOVE_CUT,
     MOVE_DEFENSE_CURL,
@@ -25818,6 +25819,7 @@ static const u16 sWatchogTeachableLearnset[] = {
 static const u16 sLillipupTeachableLearnset[] = {
     MOVE_AERIAL_ACE,
     MOVE_ATTRACT,
+    MOVE_BODY_SLAM,
     MOVE_CRUNCH,
     MOVE_DIG,
     MOVE_DOUBLE_TEAM,
@@ -25847,6 +25849,7 @@ static const u16 sLillipupTeachableLearnset[] = {
 static const u16 sHerdierTeachableLearnset[] = {
     MOVE_AERIAL_ACE,
     MOVE_ATTRACT,
+    MOVE_BODY_SLAM,
     MOVE_CRUNCH,
     MOVE_DIG,
     MOVE_DOUBLE_TEAM,
@@ -25877,6 +25880,7 @@ static const u16 sHerdierTeachableLearnset[] = {
 static const u16 sStoutlandTeachableLearnset[] = {
     MOVE_AERIAL_ACE,
     MOVE_ATTRACT,
+    MOVE_BODY_SLAM,
     MOVE_CRUNCH,
     MOVE_DIG,
     MOVE_DOUBLE_TEAM,

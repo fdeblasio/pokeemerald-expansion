bb76db13acf22bfc1a963f0abde6b49665982b05
Frank <fdeblasio1004@gmail.com>
2025-02-13 18:10:17 -0500

    Expanded Foul Play learnsets

--- src/data/pokemon/egg_moves.h
+++ src/data/pokemon/egg_moves.h
@@ -6592,7 +6592,6 @@ static const u16 sSpiritombEggMoveLearnset[] = {
     MOVE_CONFIDE,
     MOVE_DESTINY_BOND,
     MOVE_DISABLE,
-    MOVE_FOUL_PLAY,
     MOVE_GRUDGE,
     MOVE_HELPING_HAND,
     MOVE_IMPRISON,

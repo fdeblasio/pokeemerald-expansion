a9c6da7155530599759b826df5889c1ccf0ebaeb
Frank <fdeblasio1004@gmail.com>
2025-09-06 17:44:50 -0400

    Rebase fixes 9/6/25

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -5373,7 +5373,7 @@ BattleScript_ElectromorphosisActivates::
 	return
 
 BattleScript_WindPowerActivates::
-	attackstring
+	printattackstring
 	pause B_WAIT_TIME_SHORT
 	call BattleScript_AbilityPopUp
 	setcharge BS_TARGET

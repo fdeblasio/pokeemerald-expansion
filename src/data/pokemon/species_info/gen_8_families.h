3cdb64c3c1c2f57e5692126113b39ae390ae657b
Frank <fdeblasio1004@gmail.com>
2025-10-01 11:53:01 -0400

    Updated Tauros, Charcadet, Riolu, Indeedee, Eiscue, Phantump, and Dhelmise lines

--- src/data/pokemon/species_info/gen_8_families.h
+++ src/data/pokemon/species_info/gen_8_families.h
@@ -5132,9 +5132,9 @@ const struct SpeciesInfo gSpeciesInfoGen8[] =
 #if P_FAMILY_FALINKS
     [SPECIES_FALINKS] =
     {
-        .baseHP        = 65,
+        .baseHP        = 75,
         .baseAttack    = 100,
-        .baseDefense   = 100,
+        .baseDefense   = 110,
         .baseSpeed     = 75,
         .baseSpAttack  = 70,
         .baseSpDefense = 60,
@@ -5476,7 +5476,7 @@ const struct SpeciesInfo gSpeciesInfoGen8[] =
         .baseDefense   = 135,
         .baseSpeed     = 70,
         .baseSpAttack  = 20,
-        .baseSpDefense = 20,
+        .baseSpDefense = 30,
         .types = MON_TYPES(TYPE_ROCK),
         .catchRate = 60,
         .expYield = 165,
@@ -5538,7 +5538,7 @@ const struct SpeciesInfo gSpeciesInfoGen8[] =
     [SPECIES_EISCUE_ICE] =
     {
         .baseHP        = 75,
-        .baseAttack    = 80,
+        .baseAttack    = 90,
         .baseDefense   = 110,
         .baseSpeed     = 50,
         .baseSpAttack  = 65,
@@ -5604,7 +5604,7 @@ const struct SpeciesInfo gSpeciesInfoGen8[] =
     [SPECIES_EISCUE_NOICE] =
     {
         .baseHP        = 75,
-        .baseAttack    = 80,
+        .baseAttack    = 90,
         .baseDefense   = 70,
         .baseSpeed     = 130,
         .baseSpAttack  = 65,

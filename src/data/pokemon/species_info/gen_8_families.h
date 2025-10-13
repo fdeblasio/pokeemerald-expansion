3c62f42b645e27d49878cf8ca09d9c25983cb8a1
Frank <fdeblasio1004@gmail.com>
2025-10-13 15:50:56 -0400

    Updated Toxel

--- src/data/pokemon/species_info/gen_8_families.h
+++ src/data/pokemon/species_info/gen_8_families.h
@@ -3362,8 +3362,7 @@ const struct SpeciesInfo gSpeciesInfoGen8[] =
         .levelUpLearnset = sToxelLevelUpLearnset,
         .teachableLearnset = sToxelTeachableLearnset,
         .eggMoveLearnset = sToxelEggMoveLearnset,
-        .evolutions = EVOLUTION({EVO_LEVEL, 30, SPECIES_TOXTRICITY_AMPED, CONDITIONS({IF_AMPED_NATURE})},
-                                {EVO_LEVEL, 30, SPECIES_TOXTRICITY_LOW_KEY, CONDITIONS({IF_LOW_KEY_NATURE})}),
+        .evolutions = EVOLUTION({EVO_LEVEL, 30, SPECIES_TOXTRICITY_AMPED}),
     },
 
     [SPECIES_TOXTRICITY_AMPED] =

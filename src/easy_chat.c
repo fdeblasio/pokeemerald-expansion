f7772ae3cc8bc365dda50b2db5aceb4d4c48d71e
Frank <fdeblasio1004@gmail.com>
2025-11-16 16:47:25 -0500

    Prevented species names from being capitalized in easy chat

--- src/easy_chat.c
+++ src/easy_chat.c
@@ -5177,7 +5177,7 @@ u8 *CopyEasyChatWord(u8 *dest, u16 easyChatWord)
     {
         u16 index = EC_INDEX(easyChatWord);
         u8 groupId = EC_GROUP(easyChatWord);
-        resultStr = StringCopyUppercase(dest, GetEasyChatWord(groupId, index));
+        resultStr = StringCopy(dest, GetEasyChatWord(groupId, index));
     }
     else
     {

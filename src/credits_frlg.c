7ef873928b7a041147c9cd3bcaf33e2dfdaf94ca
Frank <fdeblasio1004@gmail.com>
2025-12-03 20:03:15 -0500

    Decapped FRLG

--- src/credits_frlg.c
+++ src/credits_frlg.c
@@ -376,13 +376,13 @@ ALIGNED(4) const u8 gCreditsString_Script_Designer_Map_Designer[] = _("\nScript 
 ALIGNED(4) const u8 gCreditsString_Satoshi_Nohara_Shigeru_Ohmori[] = _("\n\nSatoshi Nohara\n\nShigeru Ohmori\n\n");
 ALIGNED(4) const u8 gCreditsString_Parametric_Designers[] = _("\nParametric Designers\n\n\n\n\n");
 ALIGNED(4) const u8 gCreditsString_Koji_Nishino_Tetsuji_Ohta_Shigeki_Morimoto[] = _("\n\nKoji Nishino\nTetsuji Ohta\nShigeki Morimoto\n\n");
-ALIGNED(4) const u8 gCreditsString_POKeDEX_Text[] = _("\n\nPOKéDEX Text\n\n\n\n");
+ALIGNED(4) const u8 gCreditsString_POKeDEX_Text[] = _("\n\nPokédex Text\n\n\n\n");
 ALIGNED(4) const u8 gCreditsString_Kenji_Matsushima[] = _("\n\n\nKenji Matsushima\n\n\n");
-ALIGNED(4) const u8 gCreditsString_POKeMON_Designers[] = _("POKéMON Designers\n\n\n\n\n\n");
+ALIGNED(4) const u8 gCreditsString_POKeMON_Designers[] = _("Pokémon Designers\n\n\n\n\n\n");
 ALIGNED(4) const u8 gCreditsString_Ken_Sugimori_Motofumi_Fujiwara_Shigeki_Morimoto_Hironobu_Yoshida[] = _("\nKen Sugimori\nMotofumi Fujiwara\nShigeki Morimoto\nHironobu Yoshida\n\n");
-ALIGNED(4) const u8 gCreditsString_POKeMON_Designers_2[] = _("POKéMON Designers\n\n\n\n\n\n");
+ALIGNED(4) const u8 gCreditsString_POKeMON_Designers_2[] = _("Pokémon Designers\n\n\n\n\n\n");
 ALIGNED(4) const u8 gCreditsString_Satoshi_Ohta_Asuka_Iwashita_Takao_Unno_Kanako_Eo_Aimi_Tomita[] = _("\nSatoshi Ohta\nAsuka Iwashita\nTakao Unno\nKanako Eo\nAimi Tomita\n");
-ALIGNED(4) const u8 gCreditsString_POKeMON_Designers_3[] = _("POKéMON Designers\n\n\n\n\n\n");
+ALIGNED(4) const u8 gCreditsString_POKeMON_Designers_3[] = _("Pokémon Designers\n\n\n\n\n\n");
 ALIGNED(4) const u8 gCreditsString_Atsuko_Nishida_Muneo_Saito_Rena_Yoshikawa_Jun_Okutani[] = _("\nAtsuko Nishida\nMuneo Saito\nRena Yoshikawa\nJun Okutani\n\n");
 ALIGNED(4) const u8 gCreditsString_Supporting_Programmers[] = _("Supporting Programmers\n\n\n\n");
 ALIGNED(4) const u8 gCreditsString_Teruyuki_Yoshioka_Takao_Nakano_Satoshi_Mitsuhara_Daisuke_Hoshino[] = _("\nTeruyuki Yoshioka\nTakao Nakano\n\nSatoshi Mitsuhara\nDaisuke Hoshino\n");
@@ -972,7 +972,7 @@ static s32 RollCredits(void)
         {
             sCreditsMgr->timer--;
             return 0;
-            
+
         }
         sCreditsMgr->timer = 360;
         AddTextPrinterParameterized4(sCreditsMgr->windowId, FONT_NORMAL, 0x08, 0x29, 1, 2, sTextColor_Header, 0, TITLE_TEXT);

84c56371be9c8e0494f99c6ffccf027a6151d6a8
Frank <fdeblasio1004@gmail.com>
2026-01-07 23:17:19 -0500

    Fixed Triathlete music

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -3752,7 +3752,7 @@ WALLACE_BATTLE(WALLACE_REMATCH, 90),
 #define TRIATHLETE_M_CYCLING_INFO                   \
     .trainerClass = TRAINER_CLASS_TRIATHLETE,       \
     .trainerPic = TRAINER_PIC_CYCLING_TRIATHLETE_M, \
-    .encounterMusic_gender = TRAINER_ENCOUNTER_MUSIC_FEMALE
+    .encounterMusic_gender = TRAINER_ENCOUNTER_MUSIC_MALE
 
 #define TRIATHLETE_F_RUNNING_INFO                   \
     .trainerClass = TRAINER_CLASS_TRIATHLETE,       \
@@ -3762,7 +3762,7 @@ WALLACE_BATTLE(WALLACE_REMATCH, 90),
 #define TRIATHLETE_M_RUNNING_INFO                   \
     .trainerClass = TRAINER_CLASS_TRIATHLETE,       \
     .trainerPic = TRAINER_PIC_RUNNING_TRIATHLETE_M, \
-    .encounterMusic_gender = TRAINER_ENCOUNTER_MUSIC_FEMALE
+    .encounterMusic_gender = TRAINER_ENCOUNTER_MUSIC_MALE
 
 #define TRIATHLETE_M_SWIMMING_INFO                   \
     .trainerClass = TRAINER_CLASS_TRIATHLETE,        \

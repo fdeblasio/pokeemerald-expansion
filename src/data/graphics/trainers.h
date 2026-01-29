b6d1c2555458959c2a08309ca53fd638cf0cbdcf
Frank <fdeblasio1004@gmail.com>
2026-01-28 21:13:26 -0500

    Fixes to get Emerald working again

--- src/data/graphics/trainers.h
+++ src/data/graphics/trainers.h
@@ -331,8 +331,8 @@ const u16 gTrainerPalette_SwimmerMFrlg[] = INCBIN_U16("graphics/trainers/palette
 const u32 gTrainerFrontPic_CueBallFrlg[] = INCBIN_U32("graphics/trainers/front_pics/cue_ball_frlg.4bpp.smol");
 const u16 gTrainerPalette_CueBallFrlg[] = INCBIN_U16("graphics/trainers/palettes/cue_ball_frlg.gbapal");
 
-const u32 gTrainerFrontPic_GamerFrlg[] = INCBIN_U32("graphics/trainers/front_pics/gamer_frlg.4bpp.smol");
-const u16 gTrainerPalette_GamerFrlg[] = INCBIN_U16("graphics/trainers/palettes/gamer_frlg.gbapal");
+const u32 gTrainerFrontPic_GamerFrlg[] = INCBIN_U32("graphics/trainers/front_pics/gambler_frlg.4bpp.smol");
+const u16 gTrainerPalette_GamerFrlg[] = INCBIN_U16("graphics/trainers/palettes/gambler_frlg.gbapal");
 
 const u32 gTrainerFrontPic_BeautyFrlg[] = INCBIN_U32("graphics/trainers/front_pics/beauty_frlg.4bpp.smol");
 const u16 gTrainerPalette_BeautyFrlg[] = INCBIN_U16("graphics/trainers/palettes/beauty_frlg.gbapal");
@@ -476,24 +476,6 @@ static const u8 gTrainerBackPic_None[] = INCBIN_U8("graphics/trainers/back_pics/
 
 const u32 gTrainerFrontPic_Frank[] = INCBIN_U32("graphics/trainers/front_pics/frank.4bpp.smol");
 const u16 gTrainerPalette_Frank[] = INCBIN_U16("graphics/trainers/front_pics/frank.gbapal");
-
-const u32 gTrainerFrontPic_CrushKin[] = INCBIN_U32("graphics/trainers/front_pics/crush_kin_frlg.4bpp.smol");
-const u16 gTrainerPalette_CrushKin[] = INCBIN_U16("graphics/trainers/front_pics/crush_kin_frlg.gbapal");
-
-const u32 gTrainerFrontPic_Juggler[] = INCBIN_U32("graphics/trainers/front_pics/juggler_frlg.4bpp.smol");
-const u16 gTrainerPalette_Juggler[] = INCBIN_U16("graphics/trainers/front_pics/juggler_frlg.gbapal");
-
-const u32 gTrainerFrontPic_Gambler[] = INCBIN_U32("graphics/trainers/front_pics/gambler_frlg.4bpp.smol");
-const u16 gTrainerPalette_Gambler[] = INCBIN_U16("graphics/trainers/front_pics/gambler_frlg.gbapal");
-
-const u32 gTrainerFrontPic_Tamer[] = INCBIN_U32("graphics/trainers/front_pics/tamer_frlg.4bpp.smol");
-const u16 gTrainerPalette_Tamer[] = INCBIN_U16("graphics/trainers/front_pics/tamer_frlg.gbapal");
-
-const u32 gTrainerFrontPic_Channeler[] = INCBIN_U32("graphics/trainers/front_pics/channeler_frlg.4bpp.smol");
-const u16 gTrainerPalette_Channeler[] = INCBIN_U16("graphics/trainers/front_pics/channeler_frlg.gbapal");
-
-const u32 gTrainerFrontPic_Painter[] = INCBIN_U32("graphics/trainers/front_pics/painter_frlg.4bpp.smol");
-const u16 gTrainerPalette_Painter[] = INCBIN_U16("graphics/trainers/front_pics/painter_frlg.gbapal");
 
 const u8 gTrainerBackPic_Brendan[] = INCBIN_U8("graphics/trainers/back_pics/brendan.4bpp");
 const u8 gTrainerBackPic_May[] = INCBIN_U8("graphics/trainers/back_pics/may.4bpp");

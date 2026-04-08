dbdb8f78e73cbf7113e9a7d6d211ef2c3cf26a1c
Frank <fdeblasio1004@gmail.com>
2026-04-08 15:38:13 -0400

    Updated special character vars

--- src/string_util.c
+++ src/string_util.c
@@ -490,47 +490,30 @@ static const u8 *ExpandPlaceholder_RivalName(void)
         return (IS_FRLG ? gText_ExpandedPlaceholder_Red : gText_ExpandedPlaceholder_Brendan);
 }
 
-static const u8 *ExpandPlaceholder_Version(void)
-{
-    return gText_ExpandedPlaceholder_Emerald;
-}
-
-static const u8 *ExpandPlaceholder_Aqua(void)
-{
-    return gText_ExpandedPlaceholder_Aqua;
-}
-
-static const u8 *ExpandPlaceholder_Magma(void)
-{
-    return gText_ExpandedPlaceholder_Magma;
-}
-
-static const u8 *ExpandPlaceholder_Archie(void)
-{
-    return gText_ExpandedPlaceholder_Archie;
-}
-
-static const u8 *ExpandPlaceholder_Maxie(void)
-{
-    return gText_ExpandedPlaceholder_Maxie;
-}
-
-static const u8 *ExpandPlaceholder_Kyogre(void)
-{
-    return gText_ExpandedPlaceholder_Kyogre;
-}
-
-static const u8 *ExpandPlaceholder_Groudon(void)
-{
-    return gText_ExpandedPlaceholder_Groudon;
-}
-
 static const u8 *ExpandPlaceholder_Region(void)
 {
     if (IS_FRLG)
         return gText_Kanto;
     else
         return gText_Hoenn;
+}
+
+const u8 gText_ExpandedPlaceholder_Wally[] = _("Wally");
+static const u8 *ExpandPlaceholder_Wally(void)
+{
+    return gText_ExpandedPlaceholder_Wally;
+}
+
+const u8 gText_ExpandedPlaceholder_Ralph[] = _("Ralph");
+static const u8 *ExpandPlaceholder_Ralph(void)
+{
+    return gText_ExpandedPlaceholder_Ralph;
+}
+
+const u8 gText_ExpandedPlaceholder_Peeko[] = _("Peeko");
+static const u8 *ExpandPlaceholder_Peeko(void)
+{
+    return gText_ExpandedPlaceholder_Peeko;
 }
 
 const u8 *GetExpandedPlaceholder(u32 id)
@@ -546,14 +529,10 @@ const u8 *GetExpandedPlaceholder(u32 id)
         [PLACEHOLDER_ID_STRING_VAR_3] = ExpandPlaceholder_StringVar3,
         [PLACEHOLDER_ID_KUN]          = ExpandPlaceholder_KunChan,
         [PLACEHOLDER_ID_RIVAL]        = ExpandPlaceholder_RivalName,
-        [PLACEHOLDER_ID_VERSION]      = ExpandPlaceholder_Version,
-        [PLACEHOLDER_ID_AQUA]         = ExpandPlaceholder_Aqua,
-        [PLACEHOLDER_ID_MAGMA]        = ExpandPlaceholder_Magma,
-        [PLACEHOLDER_ID_ARCHIE]       = ExpandPlaceholder_Archie,
-        [PLACEHOLDER_ID_MAXIE]        = ExpandPlaceholder_Maxie,
-        [PLACEHOLDER_ID_KYOGRE]       = ExpandPlaceholder_Kyogre,
-        [PLACEHOLDER_ID_GROUDON]      = ExpandPlaceholder_Groudon,
         [PLACEHOLDER_ID_REGION]       = ExpandPlaceholder_Region,
+        [PLACEHOLDER_ID_WALLY]        = ExpandPlaceholder_Wally,
+        [PLACEHOLDER_ID_RALPH]        = ExpandPlaceholder_Ralph,
+        [PLACEHOLDER_ID_PEEKO]        = ExpandPlaceholder_Peeko,
     };
 
     if (id >= ARRAY_COUNT(funcs))

f472369e4fad6ca48e673a97b88a5668d639b3d9
Frank <fdeblasio1004@gmail.com>
2026-04-08 16:04:18 -0400

    Updated object events

--- src/data/graphics/trainers.h
+++ src/data/graphics/trainers.h
@@ -493,7 +493,7 @@ const u8 gTrainerBackPic_OldMan[] = INCGFX_U8("graphics/trainers/back_pics/old_m
 static const u8 gTrainerBackPic_None[] = INCBIN_U8("graphics/trainers/back_pics/none.4bpp");
 
 const u32 gTrainerFrontPic_Frank[] = INCBIN_U32("graphics/trainers/front_pics/frank.4bpp.smol");
-const u32 gTrainerPalette_Frank[] = INCBIN_U32("graphics/trainers/front_pics/frank.gbapal");
+const u16 gTrainerPalette_Frank[] = INCBIN_U16("graphics/trainers/front_pics/frank.gbapal");
 
 const u8 gTrainerBackPic_Brendan[] = INCBIN_U8("graphics/trainers/back_pics/brendan.4bpp");
 const u8 gTrainerBackPic_May[] = INCBIN_U8("graphics/trainers/back_pics/may.4bpp");

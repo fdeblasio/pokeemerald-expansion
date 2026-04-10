9cee47abc7575ebb4a229e6cf0da04f71f0aa250
Frank <fdeblasio1004@gmail.com>
2026-04-10 10:23:54 -0400

    Updated bag

--- include/constants/script_menu.h
+++ include/constants/script_menu.h
@@ -223,6 +223,8 @@ enum
 #define STDSTRING_VOLCANO_BADGE    36
 #define STDSTRING_EARTH_BADGE      37
 #define STDSTRING_COINS            38
+#define STDSTRING_SPECIAL          39
+#define STDSTRING_MEDICINE         40
 
 // Dynamic Multichoice Callbacks
 

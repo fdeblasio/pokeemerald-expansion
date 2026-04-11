2975dc190bdc11d23df9f47f8df6e7a177b35c35
Frank <fdeblasio1004@gmail.com>
2026-04-11 18:13:48 -0400

    Updated wallpapers, PC, routlette, slot machine, sped-up text, time events, Trainer Tower, and Walda

--- src/text.c
+++ src/text.c
@@ -1335,7 +1335,7 @@ static u16 RenderText(struct TextPrinter *textPrinter)
     switch (textPrinter->state)
     {
     case RENDER_STATE_HANDLE_CHAR:
-        if ((JOY_HELD(A_BUTTON | B_BUTTON) && textPrinter->hasPrintBeenSpedUp) || IsPlayerTextSpeedInstant())
+        if (textPrinter->hasPrintBeenSpedUp || IsPlayerTextSpeedInstant())
             textPrinter->delayCounter = 0;
 
         if (textPrinter->delayCounter && textPrinter->textSpeed)
--- src/time_events.c
+++ src/time_events.c
@@ -9,49 +9,9 @@
 #include "script.h"
 #include "task.h"
 
-#define MIRAGE_ISLAND_SALT 0x0300714C // Adress of the mirage island var in vanilla Emerald
-
-static u32 GetMirageRnd(void)
-{
-    #if OW_USE_DAILY_SEED_FOR_VANILLA_VARIABLES == FALSE
-    u32 hi = VarGet(VAR_MIRAGE_RND_H);
-    u32 lo = VarGet(VAR_MIRAGE_RND_L);
-    return (hi << 16) | lo;
-    #else
-    rng_value_t localRngState = LocalRandomSeed(gSaveBlock1Ptr->dailySeed ^ MIRAGE_ISLAND_SALT);
-    return LocalRandom32(&localRngState);
-    #endif
-}
-
-static void SetMirageRnd(u32 rnd)
-{
-    #if OW_USE_DAILY_SEED_FOR_VANILLA_VARIABLESD == FALSE
-    VarSet(VAR_MIRAGE_RND_H, rnd >> 16);
-    VarSet(VAR_MIRAGE_RND_L, rnd);
-    #endif
-}
-
-void UpdateMirageRnd(u16 days)
-{
-    s32 rnd = GetMirageRnd();
-    while (days)
-    {
-        rnd = ISO_RANDOMIZE2(rnd);
-        days--;
-    }
-    SetMirageRnd(rnd);
-}
-
 bool8 IsMirageIslandPresent(void)
 {
-    u16 rnd = GetMirageRnd() >> 16;
-    int i;
-
-    for (i = 0; i < PARTY_SIZE; i++)
-        if (GetMonData(&gParties[B_TRAINER_PLAYER][i], MON_DATA_SPECIES) && (GetMonData(&gParties[B_TRAINER_PLAYER][i], MON_DATA_PERSONALITY) & 0xFFFF) == rnd)
-            return TRUE;
-
-    return FALSE;
+    return TRUE;
 }
 
 void UpdateShoalTideFlag(void)
@@ -61,27 +21,27 @@ void UpdateShoalTideFlag(void)
         1, // 00
         1, // 01
         1, // 02
-        0, // 03
-        0, // 04
-        0, // 05
+        1, // 03
+        1, // 04
+        1, // 05
         0, // 06
         0, // 07
         0, // 08
-        1, // 09
-        1, // 10
-        1, // 11
-        1, // 12
-        1, // 13
-        1, // 14
+        0, // 09
+        0, // 10
+        0, // 11
+        0, // 12
+        0, // 13
+        0, // 14
         0, // 15
         0, // 16
         0, // 17
         0, // 18
         0, // 19
         0, // 20
-        1, // 21
-        1, // 22
-        1, // 23
+        0, // 21
+        0, // 22
+        0, // 23
     };
 
     if (IsMapTypeOutdoors(GetLastUsedWarpMapType()))

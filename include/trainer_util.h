c3fadedbbd6d449f5853dabe7402629364922747
Frank <fdeblasio1004@gmail.com>
2026-04-11 21:53:44 -0400

    Updated trainer classes and added dynamic accuracy/power

--- include/trainer_util.h
+++ include/trainer_util.h
@@ -7,7 +7,7 @@ struct TrainerGenerator
     u8 smartTera:1;
     u8 isFrontier:1;
     u8 trainerClass;
-    u8 padding;
+    u8 trainerPic;
     u8 name[TRAINER_NAME_LENGTH + 1];
     struct OriginalTrainerId otID;
     rng_value_t localRngState;

a4428023efbf1b549a008b948534d4a871fc6449
Frank <fdeblasio1004@gmail.com>
2026-08-11 11:08:20 -0400

    Added back Houndour to Hoenn Dex

--- src/cable_car.c
+++ src/cable_car.c
@@ -783,7 +783,8 @@ static void CreateCableCarSprites(void)
         OBJ_EVENT_GFX_SPECIES(CABLE_CAR_1),
         OBJ_EVENT_GFX_SPECIES(CABLE_CAR_2),
         OBJ_EVENT_GFX_SPECIES(CABLE_CAR_3),
-        OBJ_EVENT_GFX_SPECIES(CABLE_CAR_4)
+        OBJ_EVENT_GFX_SPECIES(CABLE_CAR_4),
+        OBJ_EVENT_GFX_SPECIES(CABLE_CAR_5)
     };
     s16 hikerCoords[2][2] = {
         {   0,  80 }, // Going up

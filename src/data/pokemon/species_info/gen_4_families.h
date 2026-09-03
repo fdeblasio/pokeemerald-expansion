e0d557b3fa3546f135878874a188fb5be4e6583e
Frank <fdeblasio1004@gmail.com>
2026-09-02 22:38:29 -0400

    Gave Steel Wing to Gen IX mons

--- src/data/pokemon/species_info/gen_4_families.h
+++ src/data/pokemon/species_info/gen_4_families.h
@@ -4123,7 +4123,7 @@ const struct SpeciesInfo gSpeciesInfoGen4[] =
         .baseSpAttack  = 95,
         .baseSpDefense = 45,
         .types = MON_TYPES(TYPE_NORMAL, TYPE_FLYING),
-        .catchRate = DEFAULT_CATCH_RATE(30),
+        .catchRate = BOOSTED_CATCH_RATE(30),
         .expYield = (P_UPDATED_EXP_YIELDS >= GEN_5) ? 144 : 107,
         .evYield_Attack = 1,
         .itemRare = ITEM_METRONOME,
